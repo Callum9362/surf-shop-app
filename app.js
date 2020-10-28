1bf743a27394512a8356049a3789f4ba71e864ff
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-10-28 17:55:11 +1100

    Added reviews routes

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const postsRouter = require('./routes/posts')
+const postsRouter = require('./routes/posts');
+const reviewsRouter = require('./routes/reviews');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts/:id/reviews', reviewsRouter);
 app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
